10dedce37c3a3edac2424c1f0d23b7a81f84a707
Patrick-Jun <nunet@qq.com>
2020-11-03 23:53:12 +0800

    v0.1.0 [F]千分位错误；[A]支持小数；

--- js/jquery.rollNumber.js
+++ js/jquery.rollNumber.js
@@ -2,14 +2,14 @@
  * @Author: Patrick-Jun
  * @Date: 2020-08-03 11:21:42
  * @Last Modified by: Patrick-Jun
- * @Last Modified time: 2020-08-03 11:29:30
+ * @Last Modified time: 2020-11-03 23:49:34
  * @Git: https://github.com/Patrick-Jun/jQuery.rollNumber.git
  */
 
 (function($) {
   $.fn.rollNumber = function(options) {
     let $self = this;
-    if(options.number === undefined) return;
+    if (options.number === undefined) return;
     let number = options.number,
         speed = options.speed || 500,
         interval = options.interval || 100,
@@ -34,34 +34,51 @@
     let numberHtml = '';
     for (let i = 0; i < 10; i++) numberHtml += `<span style="display: block; width: ${ space }px; height: ${ _height }px; line-height: ${ _height }px; text-align: center; ${ Object.keys(fontStyle).join(': inherit; ') + ': inherit;' }">${ i }</span>`;
     numberHtml = `<div class="_number" style="width: ${ space }px; height: ${ _height }px; line-height: ${ _height }px; display: flex; justify-content: center; align-items: center;"><div style="position: relative; width: ${ space }px; height: ${ _height }px; overflow: hidden;"><div style="position: absolute; width: 100%;">${ numberHtml }</div></div></div>`
+    
+    // 处理数字
+    let numArr = String(number).split('');
+    if (_fillZero) { // 前置补0
+      // 当含有小数时，补0位数应该+1
+      if (String(number).indexOf('.') !== -1) rooms++;
+      for (let i = numArr.length; i < rooms; i++) {
+        numArr.unshift(0);
+      }
+      number = numArr.join('');
+    }
     if (!!options.symbol) { // 含千分位
       let appendHtml = [];
       let symbolHtml = `<span style="display: block; width: ${ space }px; height: ${ _height }px; line-height: ${ _height }px; text-align: center; ${ Object.keys(fontStyle).join(': inherit; ') + ': inherit;' }">${ options.symbol }</span>`;
+      let dotHtml = `<span style="display: block; width: ${ space }px; height: ${ _height }px; line-height: ${ _height }px; text-align: center; ${ Object.keys(fontStyle).join(': inherit; ') + ': inherit;' }">.</span>`;
       symbolHtml = `<div class="_number" style="width: ${ space }px; height: ${ _height }px; line-height: ${ _height }px; display: flex; justify-content: center; align-items: center;"><div style="position: relative; width: ${ space }px; height: ${ _height }px; overflow: hidden;"><div style="position: absolute; width: 100%;">${ symbolHtml }</div></div></div>`;
-      for (let i = 0; i < rooms; i++) {
-        appendHtml.push(numberHtml);
-        if(rooms > 3 && i != 0 && (rooms - i) != 1 && (rooms - i)%3 == 1) {
-          appendHtml.push(symbolHtml);
+      dotHtml = `<div class="_number" style="width: ${ space }px; height: ${ _height }px; line-height: ${ _height }px; display: flex; justify-content: center; align-items: center;"><div style="position: relative; width: ${ space }px; height: ${ _height }px; overflow: hidden;"><div style="position: absolute; width: 100%;">${ dotHtml }</div></div></div>`;
+      
+      let numarr = String(number).split('.');
+      const re = /(-?\d+)(\d{3})/;
+      while (re.test(numarr[0])) {
+        numarr[0] = numarr[0].replace(re, '$1,$2');
+      }
+      numArr = (numarr.length > 1 ? numarr[0] + '.' + numarr[1] : numarr[0]).split('');
+      for (let i = 0; i < numArr.length; i++) {
+        if (isNaN(Number(numArr[i]))) { // 判断是否是符号
+          if (numArr[i] === '.') { // 判断小数
+            appendHtml.push(dotHtml);
+          } else {
+            appendHtml.push(symbolHtml);
+          }
+        } else {
+          appendHtml.push(numberHtml);
         }
       }
       $self.append(appendHtml.join('')).css(fontStyle);
     }else {
       $self.append(numberHtml.repeat(rooms)).css(fontStyle);
-    }
-    // 处理数字
-    let numArr = (number+'').split('');
-    if (_fillZero) { // 前置补0
-      for (let i = numArr.length; i < rooms; i++) {
-        numArr.unshift(0);
+      // 处理小数符号
+      if (String(number).indexOf('.') !== -1) {
+        $($self.find('._number')[String(number).indexOf('.')]).find('span')[0].innerHTML = '.';
       }
     }
+
     let domArr = $self.find('._number');
-    if(!!options.symbol) { // 含千分位：重新处理数字，增加千分位占位
-      numArr = numArr.reverse().reduce((prev, next, index) => {
-        return ((index % 3) ? next : (next + options.symbol)) + prev;
-      })
-      numArr = numArr.split('');
-    }
 
     for (let i = 0; i < domArr.length; i++) {
       setTimeout(function(dom, n) {
